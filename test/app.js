2b7ece2c06e01952d03263228197acd8a93e2995
Chris Atkin <atkinchris@gmail.com>
2016-09-19 15:13:01 +0100

    Add test Angular app

--- test/app.js
+++ test/app.js
@@ -1,4 +1,20 @@
 var angular = require('angular');
 var adalAngular = require('../adal-angular');
 
-console.log(adalAngular);
+function config(adalAuthenticationServiceProvider, $httpProvider) {
+  adalAuthenticationServiceProvider.init({ clientId: 'example-client-id' }, $httpProvider);
+}
+
+function controller($scope, adalAuthenticationService) {
+  $scope.login = function () {
+      adalAuthenticationService.login();
+  };
+}
+
+angular.module('app', [adalAngular])
+  .config(config)
+  .controller('ctrl', controller);
+
+angular.element(document).ready(function() {
+  angular.bootstrap(document, ['app']);
+});
