7cea2104c8e05231beb0dc78f94e0f2c2412563d
Chris Atkin <atkinchris@gmail.com>
2016-09-20 22:20:27 +0100

    Add comments to explain module

--- adal-angular.js
+++ adal-angular.js
@@ -1,5 +1,8 @@
+// Expose the AuthenticationContext import on the window object.
+// This makes it available for iframes calling window.parent.AuthenticationContext.
 window.AuthenticationContext = require('adal-angular');
 
 require('adal-angular/lib/adal-angular');
 
+// Export the Angular module name, as hardcoded in the adal-angular package.
 module.exports = 'AdalAngular';
